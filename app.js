12604e340a40c1f7a2196fbe408e5e3d47cf83be
RajeshSR0809 <srrajesh.svpur@gmail.com>
2022-10-10 20:44:23 +0530

    integrating commander

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ import commander from 'commander';
 import csrf from 'csurf';
 import express from 'express';
 
-import parser from  'mongodb-query-parser';
-import EJSON from  'mongodb-extended-json';
-var query = '{_id: ObjectId("58c33a794d08b991e3648fd2")}';
-var queryAsAnObjectWithTypes = parser.parseFilter(query);
-var a = EJSON.stringify(queryAsAnObjectWithTypes);
+// import parser from  'mongodb-query-parser';
+// import EJSON from  'mongodb-extended-json';
+// var query = '{_id: ObjectId("58c33a794d08b991e3648fd2")}';
+// var queryAsAnObjectWithTypes = parser.parseFilter(query);
+// var a = EJSON.stringify(queryAsAnObjectWithTypes);
 
-console.log(a);
+// console.log(a);
+
+
+commander
+  .version(pkg.version)
+  .option('-U, --url <url>', 'connection string url')
+  .option('-H, --host <host>', 'hostname or address of the db(deprecated)')
+  .option('-P, --dbport <host>', 'port of the db(deprecated)')
+  .option('-u, --username <username>', 'username for authentication(deprecated)')
+  .option('-p, --password <password>', 'password for authentication(deprecated)')
+  .option('-a, --admin', 'enable authentication as admin')
+  .option('-d, --database <database>', 'authenticate to database')
+  .option('--port <port>', 'listen on specified port')
+  .parse(process.argv);
